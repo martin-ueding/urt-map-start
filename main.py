6e7775d052f60a73ec23a1d0da615ec432ad8452
Martin Ueding <dev@martin-ueding.de>
2011-02-09 23:09:03 +0100

    generalized filename

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main ():
 	maps = glob.glob('/home/mu/.q3a/q3ut4/*.pk3')
 	mapnames = []
 	for map in maps:
-		m = re.search('(?:.*/)+([A-Za-z0-9_\\.]+)\\.pk3', map)
+		m = re.search('(?:.*/)+([^/]+)\\.pk3', map)
 		mapnames.append(m.group(1))
 	print mapnames
 
