8285c1d744ce711cfeb205ed8e0c3f242b728e1c
Martin Ueding <dev@martin-ueding.de>
2011-02-12 15:10:45 +0100

    added GUI

--- main.py
+++ main.py
@@ -3,17 +3,37 @@ import glob
 import re
 import os
 import random
+from PyQt4 import QtCore, QtGui
+from listui import Ui_Dialog
+import sys
 
-def main ():
+mapnames = []
+	
+def printRandomMapName ():
+	print random.choice(mapnames)
+
+class MyForm(QtGui.QMainWindow):
+  def __init__(self, parent=None):
+	QtGui.QWidget.__init__(self, parent)
+	self.ui = Ui_Dialog()
+	self.ui.setupUi(self)
+	QtCore.QObject.connect(self.ui.startButton, QtCore.SIGNAL("clicked()"), self.printSelectedMap)
 	maps = glob.glob('/home/mu/.q3a/q3ut4/*.pk3')
-	mapnames = []
 	for map in maps:
 		m = re.search('(?:.*/)+([^/]+)\\.pk3', map)
 		mapnames.append(m.group(1))
-	print mapnames
+		self.ui.mapList.addItem(m.group(1))
 
-	os.system('/opt/UrbanTerror/ioUrbanTerror.i386 +map '+random.choice(mapnames))
+  def printSelectedMap (self):
+	row = self.ui.mapList.currentRow()
+	print mapnames[row]
+
+	os.system('/opt/UrbanTerror/ioUrbanTerror.i386 +map '+mapnames[row])
 
 
 if __name__ == "__main__":	
-	main()
+	app = QtGui.QApplication(sys.argv)
+	myapp = MyForm()
+	myapp.show()
+	sys.exit(app.exec_())
+
---
+++ makefile
@@ -0,0 +1,2 @@
+listui.py: list.ui
+	pyuic4 list.ui -o listui.py
